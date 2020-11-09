84cbd07de83592e4727a8e26c7f4977b4840094e
Y0h4n3s <yohaksa80@gmail.com>
2020-11-09 17:37:04 -0500

    .

--- frontend/src/commons/routes.js
+++
@@ -1,5 +0,0 @@
-
-export const LOGIN = "/login"
-export const SIGN_UP = "/sign-up"
-export const ACCOUNT = "/account"
-export const HOME = "/"
--- frontend/src/components/Firebase/index.js
+++
@@ -1,6 +0,0 @@
-import FirebaseConfig from "./Config"
-import FirebaseContext from "./Context"
-import Firebase from "./Firebase"
-export default FirebaseConfig
-export {FirebaseContext}
-export default Firebase
